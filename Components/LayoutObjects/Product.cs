4558817cd7c2e77c84150f19075c0d2b321b9176
Julian Rich <123442863+ric394@users.noreply.github.com>
2026-03-16 16:28:11 +1000

    Projected created with some pages

---
+++ Components/LayoutObjects/Product.cs
@@ -0,0 +1,10 @@
+
+namespace APSIM.RegistrationV2.Components.LayoutObjects
+{
+    public class Product
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string ImageUrl { get; set; }
+    }
+}
